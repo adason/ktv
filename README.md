35a14d465bbf576ff25baad8e9a8cf3728e6d079
adason <tdadason@gmail.com>
2020-05-01 00:41:33 -0700

    Add features to combine with video

--- README.md
+++ README.md
@@ -3,20 +3,28 @@ Split vocal and accompaniment for a given youtube URL.
 
 ## Download and split using docker
 
-For example, to split this [song](https://www.youtube.com/watch?v=1hI-7vj2FhE)
+For example, to split this [song](https://www.youtube.com/watch?v=51SOZjKuaaQ)
 
 ```
 docker run \
   -e MODEL_PATH=/tmp/ktv/models \
   -v /tmp:/tmp \
   -t adason/ktv:latest \
-  -o quiet \
-  https://www.youtube.com/watch?v=1hI-7vj2FhE
+  -o luggage \
+  https://www.youtube.com/watch?v=51SOZjKuaaQ
 ```
 
 You can find downloaded mp3 file as well as split files under `/tmp/ktv`.
 
 ## Development
+
+### Python virtualenv
+
+```
+python -m ktv -o output https://www.youtube.com/watch?v=VpwAq7hiij0
+```
+
+### Docker
 
 ```
 docker build -t ktv .
@@ -25,8 +33,8 @@ docker run \
   -e MODEL_PATH=/tmp/ktv/models \
   -v /tmp:/tmp \
   -t ktv \
-  -o quiet \
-  https://www.youtube.com/watch?v=1hI-7vj2FhE
+  -o luggage \
+  https://www.youtube.com/watch?v=51SOZjKuaaQ
 ```
 
 Push to docker registry
--- ktv/tasks.py
+++ ktv/tasks.py
@@ -1,28 +1,25 @@
 from pathlib import Path
 import logging
 
+import ffmpeg
 from spleeter.separator import Separator
 import youtube_dl
+
+import ktv.constants as const
 
 
 logger = logging.getLogger(__name__)
 
 
-def download(url, output):
+def download(url, filename):
+    filename_ext = f"{filename}.{const.VIDEO_EXTENSION}"
     ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
+        "format": f"bestvideo[ext={const.VIDEO_EXTENSION}]/best",
         "logger": logger,
-        "outtmpl": output
+        "outtmpl": filename_ext
     }
 
-    logger.info(f"Downloading from {url} and convert into {output}")
+    logger.info(f"Downloading from {url} and save into {filename_ext}")
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         retcode = ydl.download([url])
@@ -32,7 +29,30 @@ def download(url, output):
     return retcode
 
 
-def split(file):
+def extract_audio(filename):
+    in_video = f"{filename}.{const.VIDEO_EXTENSION}"
+    out_audio = f"{filename}.{const.AUDIO_EXTENSION}"
+    ffmpeg.input(in_video).output(out_audio).overwrite_output().run()
+
+
+def split(filename):
+    filename_ext = f"{filename}.{const.AUDIO_EXTENSION}"
     separator = Separator("spleeter:2stems")
-    logger.info(f"Splitting file {file}")
-    separator.separate_to_file(file, Path(file).parent)
+    logger.info(f"Splitting file {filename_ext}")
+    separator.separate_to_file(filename_ext, Path(filename).parent)
+
+
+def combine(filename):
+    in_video = f"{filename}.{const.VIDEO_EXTENSION}"
+    out_video = f"{filename}_processed.{const.VIDEO_EXTENSION}"
+    vocals = str(Path(filename) / "vocals.wav")
+    accompaniment = str(Path(filename) / "accompaniment.wav")
+    combined = ffmpeg.output(
+        ffmpeg.input(in_video),
+        ffmpeg.input(accompaniment),
+        ffmpeg.input(vocals),
+        filename=out_video
+    )
+    combined.overwrite_output().run()
+
+
